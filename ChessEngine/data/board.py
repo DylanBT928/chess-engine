ef7a3dabb7e677f33168395e76dfa9de0cff8563
Dylan Tran <dylanbt928@gmail.com>
2025-02-09 00:23:40 -0800

    Created class for Board

--- ChessEngine/data/board.py
+++ ChessEngine/data/board.py
@@ -1,7 +1,16 @@
 import pygame
 
-def draw_board(screen: pygame.display):
-    for x in range(8):
-        for y in range(8):
-            color = (237, 242, 239) if (x + y) % 2 == 0 else (97, 155, 138)
-            pygame.draw.rect(screen, color, [(x * 64) + 25, (y * 64) + 25, 64, 64])
+class Board:
+    def __init__(self):
+        """Creates a chess board."""
+        self.board = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+        self.rows = 8
+        self.cols = 8
+
+
+    def draw_board(self, screen: pygame.display):
+        """Displays the board onto the screen."""
+        for x in range(self.rows):
+            for y in range(self.cols):
+                color = (237, 242, 239) if (x + y) % 2 == 0 else (97, 155, 138)
+                pygame.draw.rect(screen, color, [(x * 64) + 25, (y * 64) + 25, 64, 64])
--- ChessEngine/main.py
+++ ChessEngine/main.py
@@ -1,12 +1,19 @@
 import pygame
 
-from ChessEngine.data.board import draw_board
+from ChessEngine.data.board import Board
+
+SCREEN_WIDTH = 562
+SCREEN_HEIGHT = 562
 
 # Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode((562, 562))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Chess Engine')
 clock = pygame.time.Clock()
 running = True
+
+# Initialize game
+board = Board()
 
 # Main loop
 while running:
@@ -14,11 +21,14 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     screen.fill((36, 35, 37))
 
     # RENDER GAME
-    draw_board(screen)
+    board.draw_board(screen)
 
     # Update the display
     pygame.display.flip()
